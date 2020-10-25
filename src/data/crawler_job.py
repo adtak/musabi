270db91f741edc5da0aac63460bd900b262a2065
adtak <adtak62@gmail.com>
2020-10-25 23:13:56 +0900

    feat: add crawler

---
+++ src/data/crawler_job.py
@@ -0,0 +1,21 @@
+import argparse
+
+from src.image_crawler import ImageCrawler
+
+
+def main():
+    args = parse_arguments()
+    output_dir = args.output
+    _ = ImageCrawler(output_dir).run(args.keyword, args.max)
+
+
+def parse_arguments():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-o", "--output", required=True)
+    arg_parser.add_argument("-k", "--keyword", required=True)
+    arg_parser.add_argument("-m", "--max", required=True, type=int)
+    return arg_parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/data/image_crawler.py
@@ -0,0 +1,27 @@
+from icrawler.builtin import BingImageCrawler
+import os
+from typing import List
+
+
+class ImageCrawler(object):
+    def __init__(self, output_dir: str) -> None:
+        self.output_dir = output_dir
+        self.crawler = BingImageCrawler(storage={"root_dir": output_dir})
+
+    def run(self, keyword: str, max_num: int) -> List[str]:
+        filters = dict(
+            type="photo",
+            size="medium",
+            license="creativecommons",
+            layout="tall")
+
+        self.crawler.crawl(keyword=keyword, filters=filters, max_num=max_num)
+
+        return self.get_results()
+
+    def get_results(self) -> List[str]:
+        return os.listdir(self.output_dir)
+
+
+if __name__ == "__main__":
+    pass
