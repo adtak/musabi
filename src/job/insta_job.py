00f8e580ac19eed3b26f05fccff8b77ed262666b
adtak <adtak62@gmail.com>
2020-11-24 19:20:05 +0900

    fix: update insta job

--- src/job/insta_job.py
+++ src/job/insta_job.py
@@ -11,7 +11,7 @@ def main():
     args = parse_arguments()
     img_name = "gen_img.jpg"
 
-    generate_image(args.img_dir_path, img_name)
+    generate_image(args.model_dir_path, args.img_dir_path, img_name)
 
     tags = [""]
     caption = add_hashtag(args.caption, tags)
@@ -23,17 +23,18 @@ def main():
 
 def parse_arguments():
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-m", "--model_dir_path", required=True)
     arg_parser.add_argument("-i", "--img_dir_path", required=True)
     arg_parser.add_argument("-c", "--caption")
     return arg_parser.parse_args()
 
 
-def generate_image(img_dir_path: str, img_name: str) -> None:
-    model = TrainedModel()
+def generate_image(model_dir_path: str, img_dir_path: str, img_name: str) -> None:
+    model = TrainedModel(model_dir_path)
     model.load()
     img = model.predict()
     # save_image
-    image_util.save_image(img, img_dir_path, img_name)
+    image_util.save_image(img[0], img_dir_path, img_name)
 
 
 def add_hashtag(caption: str, tags: List[str]) -> str:
