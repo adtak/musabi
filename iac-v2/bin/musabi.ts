44bc36d73fd6265e64b638fa671de1b5f57c3020
adtak <adtak62@gmail.com>
2025-04-23 18:55:56 +0900

    rename iac filename

--- iac-v2/bin/musabi.ts
+++ iac-v2/bin/musabi.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { IacV2Stack } from "../lib/iac-v2-stack";
+import { MusabiStack } from "../lib/musabi-stack";
 
 const app = new cdk.App();
-new IacV2Stack(app, "IacV2Stack", {
+new MusabiStack(app, "MusabiStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- iac-v2/lib/musabi-stack.ts
+++ iac-v2/lib/musabi-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import type { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
-export class IacV2Stack extends cdk.Stack {
+export class MusabiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
