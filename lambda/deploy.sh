50bb20df0e577310865c511864a91e4d851cb9e4
adtak <adtak62@gmail.com>
2025-04-27 17:49:43 +0900

    update to use --group installing deps in Dockerfile

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -9,12 +9,20 @@ fi
 AWS_ACCOUNT_ID="$1"
 FUNC_NAME="$2"
 
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com"
+aws ecr get-login-password --region ap-northeast-1 \
+  | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com"
 
 IMAGE_TAG="musabi-${FUNC_NAME}"
 ECR_REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/musabi-${FUNC_NAME}"
 
-docker buildx build --platform linux/amd64 --provenance=false --target "${FUNC_NAME}" -t "${IMAGE_TAG}:latest" -f docker/Dockerfile .
+docker buildx build \
+  --platform linux/amd64  \
+  --provenance=false \
+  --build-arg "INSTALL_GROUP=${FUNC_NAME}" \
+  --target "${FUNC_NAME}" \
+  -t "${IMAGE_TAG}:latest" \
+  -f docker/Dockerfile \
+  .
 docker tag "${IMAGE_TAG}:latest" "${ECR_REPO_URI}:latest"
 docker push "${ECR_REPO_URI}:latest"
 
