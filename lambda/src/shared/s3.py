fea7fad1bcdad30adb72041f9db8a122b524e3bc
adtak <adtak62@gmail.com>
2025-06-25 00:48:44 +0900

    add get_image shared function

--- lambda/src/shared/s3.py
+++ lambda/src/shared/s3.py
@@ -4,6 +4,13 @@ import boto3
 from PIL import Image
 
 
+def get_image(bucket_name: str, s3_object_key: str) -> Image.Image:
+    s3_client = boto3.client("s3")
+    response = s3_client.get_object(Bucket=bucket_name, Key=s3_object_key)
+    image_bytes = response["Body"].read()
+    return Image.open(io.BytesIO(image_bytes))
+
+
 def put_image(image: Image.Image, bucket_name: str, s3_object_key: str) -> str:
     image_buffer = io.BytesIO()
     image.save(image_buffer, format="PNG")
